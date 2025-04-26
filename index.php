47e48cd4f576ffdc5d45454dbe6a36527c028cda
laurenpatriciaa <laurenpatricia166@gmail.com>
2025-04-26 10:47:43 +0700

    menampilkan notifikasi barang habis

--- index.php
+++ index.php
@@ -58,6 +58,24 @@ require 'cek.php';
                             </button>
                             </div>
                             <div class="card-body">
+                            
+                            <?php
+                                $ambildatastock = mysqli_query($conn,"select * from stock where stock < 1");
+
+                                while($fetch=mysqli_fetch_array($ambildatastock)){
+                                    $barang = $fetch['namabarang'];
+                                
+
+                            ?>
+                            <div class="alert alert-danger alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                                <strong>Perhatian!</strong> Stock Barang <?=$barang;?> Telah Habis
+                            </div>
+
+                            <?php
+                                }
+                            ?>
+
                                 <div class="table-responsive">
                                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                         <thead>
